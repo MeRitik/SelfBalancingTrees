bbfb9e5cdc755e8f5d3895984780459c2e5487aa
Ritik Ranjan <ritikranjan999@gmail.com>
2026-02-22 20:04:02 +0530

    Implement SelfBalancingBST and AVLTree classes with basic operations

---
+++ AVLTree.hpp
@@ -0,0 +1,108 @@
+#include "SelfBalancingBST.hpp"
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+template <typename T, typename Compare = less<T>>
+class AVLTree : public SelfBalancingBST<T, Compare> {
+private:
+  using Node = typename SelfBalancingBST<T, Compare>::Node;
+
+  int balance_factor(Node *node) {
+    return this->height(node->left) - this->height(node->right);
+  }
+
+  Node *balance(Node *node) override {
+    if (!node)
+      return node;
+
+    this->update_height(node);
+
+    int bf = balance_factor(node);
+
+    // LEFT HEAVY
+    if (bf > 1) {
+
+      // LL Case
+      if (balance_factor(node->left) >= 0)
+        return this->rotate_right(node);
+
+      // LR Case
+      node->left = this->rotate_left(node->left);
+      return this->rotate_right(node);
+    }
+
+    // RIGHT HEAVY
+    if (bf < -1) {
+
+      // RR Case
+      if (balance_factor(node->right) <= 0)
+        return this->rotate_left(node);
+
+      // RL Case
+      node->right = this->rotate_right(node->right);
+      return this->rotate_left(node);
+    }
+
+    return node; // already balanced
+  }
+
+  Node *insert(Node *node, const T &value) override {
+
+    if (!node)
+      return new Node(value);
+
+    if (this->comp(value, node->data))
+      node->left = insert(node->left, value);
+    else
+      node->right = insert(node->right, value);
+
+    return balance(node);
+  }
+
+  Node *find_min(Node *node) {
+    while (node->left)
+      node = node->left;
+    return node;
+  }
+
+  Node *remove(Node *node, const T &value) override {
+    if (!node)
+      return node;
+
+    if (this->comp(value, node->data))
+      node->left = remove(node->left, value);
+
+    else if (this->comp(node->data, value))
+      node->right = remove(node->right, value);
+
+    else {
+      if (!node->left || !node->right) {
+
+        Node *temp = node->left ? node->left : node->right;
+
+        if (!temp) {
+          temp = node;
+          node = nullptr;
+        } else {
+          *node = *temp;
+        }
+
+        delete temp;
+      }
+
+      else {
+        Node *successor = find_min(node->right);
+        node->data = successor->data;
+        node->right = remove(node->right, successor->data);
+      }
+    }
+
+    if (!node)
+      return node;
+
+    return balance(node);
+  }
+};
---
+++ SelfBalancingBST.hpp
@@ -0,0 +1,99 @@
+#ifndef SELF_BALANCING_BST_HPP
+#define SELF_BALANCING_BST_HPP
+
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+template <typename T, typename Compare = less<T>> class SelfBalancingBST {
+protected:
+  struct Node {
+    T data;
+    Node *left;
+    Node *right;
+    int height;
+
+    Node(const T &value)
+        : data(value), left(nullptr), right(nullptr), height(1) {}
+  };
+
+  Node *root;
+  Compare comp;
+
+  virtual Node *insert(Node *node, const T &value) = 0;
+  virtual Node *remove(Node *node, const T &value) = 0;
+  virtual Node *balance(Node *node) = 0;
+
+  int height(Node *node) const { return node ? node->height : 0; }
+
+  void update_height(Node *node) {
+    if (!node)
+      return;
+
+    node->height = 1 + max(height(node->left), height(node->right));
+  }
+
+  Node *rotate_right(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    update_height(y);
+    update_height(x);
+
+    return x;
+  }
+
+  Node *rotate_left(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    update_height(x);
+    update_height(y);
+
+    return y;
+  }
+
+  bool search(Node *node, const T &value) const {
+    if (!node)
+      return false;
+
+    if (!comp(value, node->data) && !comp(node->data, value))
+      return true;
+
+    if (comp(value, node->data))
+      return search(node->left, value);
+
+    return search(node->right, value);
+  }
+
+  void inorder(Node *node) const {
+    if (!node)
+      return;
+
+    inorder(node->left);
+    cout << node->data << " ";
+    inorder(node->right);
+  }
+
+public:
+  SelfBalancingBST() : root(nullptr) {}
+
+  void insert(const T &value) { root = insert(root, value); }
+
+  void remove(const T &value) { root = remove(root, value); }
+
+  bool search(const T &value) const { return search(root, value); }
+
+  void print_inorder() const {
+    inorder(root);
+    cout << endl;
+  }
+};
+
+#endif
